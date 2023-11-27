d0053a9f0bd048564f7dc5dbcd66f6d28a9c3516
bunny254 <kimaniwachira03@gmail.com>
2023-11-27 08:42:03 +0300

    Added Birthday Confetti to my portfolio

--- client/src/siteComponents/landingPage.jsx
+++ client/src/siteComponents/landingPage.jsx
@@ -7,19 +7,21 @@ import { useDarkMode } from "./darkMode";
 import Projects from "./projects";
 import Events from "./events";
 import Testimonials from "./testimonials";
+import Confetti from "react-confetti";
 
 const LandingPage = () => {
-  const { isDarkMode} = useDarkMode();
+  const { isDarkMode } = useDarkMode();
   return (
     <div className={isDarkMode ? "dark" : ""}>
-      <div className="dark:bg-gray-900" >
-      <Nav/>
-      <About/>
-      <Home/>
-      <Skills/>
-      <Projects/>
-      <Events/>
-      <Testimonials/>
+      <div className="dark:bg-gray-900">
+          <Nav />
+          <Confetti width={1900} height={1000}/>
+          <About />
+          <Home />
+          <Skills />
+          <Projects />
+          <Events />
+          <Testimonials />
       </div>
     </div>
   );
